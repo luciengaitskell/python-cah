321f37fbd719af205b289cc05c678f11ea744d3f
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2017-04-11 07:42:59 -0400

    Add 'cah' module, which holds the main Game class

---
+++ cah/cah.py
@@ -0,0 +1,64 @@
+from . import cards, player
+
+
+MAX_CARDS_PER_PLAYER = 5
+
+
+class Game:
+    curr_question = None
+
+    questions = cards.CardGroup(cards.questions)
+    answers = cards.CardGroup(cards.answers)
+
+    next_player_id = 0
+    players = []
+
+    player_cards = {}
+
+    def __init__(self):
+        pass
+
+    def add_player_id(self, player_id):
+        new_player = player.Player(player_id)
+        self.players.append(new_player)
+        return new_player
+
+    def add_player(self):
+        player_id = self.next_player_id
+        self.next_player_id += 1
+        return player_id, self.add_player_id(player_id)
+
+    def get_new_question(self):
+        self.curr_question = self.questions.get_new_card_random()
+        return self.curr_question
+
+    def deal_cards(self):
+        num_cards = len(self.answers.cards)
+
+        deal_num = int(num_cards/len(self.players))
+
+        if deal_num > MAX_CARDS_PER_PLAYER:
+            deal_num = MAX_CARDS_PER_PLAYER
+        elif deal_num < 1:
+            raise KeyError("Not enough cards to continue play.")
+
+        for t in range(0, deal_num):
+            for p in self.players:
+                new_card = self.answers.get_new_card_random()
+                p.cards[new_card[0]] = new_card[1]
+
+    def reset_player_cards(self):
+        self.player_cards = {}
+
+    def set_player_card(self, plyr, card_id):
+        card_data = plyr.select_card(card_id)
+        self.player_cards[plyr] = (card_id, card_data)
+
+    def get_player_no_card(self):
+        players_with_cards = list(self.player_cards.keys())
+        return [x for x in self.players if x not in players_with_cards]
+
+    def set_round_winner(self, plyr):
+        answer = self.player_cards[plyr]
+        opponent_answers = [value for key, value in self.player_cards.items() if key not in [plyr]]
+        plyr.add_win(self.curr_question[0], answer, opponent_answers)
