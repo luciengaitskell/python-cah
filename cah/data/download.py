c93439b99d9a1ab48eb62444b401618b902e7110
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2017-04-11 08:42:20 -0400

    Fix relative paths for 'answer.txt' and 'question.txt' for importing

--- cah/data/download.py
+++ cah/data/download.py
@@ -1,4 +1,6 @@
 from urllib.request import urlopen
+import os
+
 
 def download_card_list(url):
     start_delete_str = 'cards='
@@ -8,10 +10,10 @@ def download_card_list(url):
     data = data[start_index:]
     return data
 
-with open("bin/answer.txt", "w") as f:
+with open(os.path.join(os.path.dirname(__file__), "bin/answer.txt"), "w") as f:
     f.write(download_card_list("http://www.cardsagainsthumanity.com/wcards.txt"))
 
-with open("bin/question.txt", "w") as f:
+with open(os.path.join(os.path.dirname(__file__), "bin/question.txt"), "w") as f:
     f.write(download_card_list("http://www.cardsagainsthumanity.com/bcards.txt"))
     f.write(download_card_list("http://www.cardsagainsthumanity.com/bcards1.txt"))
     f.write(download_card_list("http://www.cardsagainsthumanity.com/bcards2.txt"))
