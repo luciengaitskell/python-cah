8a6ce38ed907039d55074be6056096df2e7bf6c4
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2017-04-11 07:36:54 -0400

    Add 'player' module

---
+++ cah/player.py
@@ -0,0 +1,21 @@
+class Player:
+    id = None
+
+    cards = None
+    wins = None
+
+    def __init__(self, player_id):
+        self.cards = {}
+        self.wins = {}
+        self.id = player_id
+
+    def add_win(self, question, answer, opponent_answers):
+        self.wins[question] = {'answer': answer, 'opponent_answers': opponent_answers}
+
+    def select_card(self, card_id):
+        if card_id not in self.cards:
+            raise KeyError("Card ID not in possession of this Player.")
+
+        crd = self.cards[card_id]
+        del(self.cards[card_id])
+        return crd
